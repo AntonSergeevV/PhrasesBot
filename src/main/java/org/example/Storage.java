36da4175df739d68a65da4d8197ad7225ec87c87
Антон <sergeev.tony@ya.ru>
2023-09-25 20:19:55 +0300

    some change

--- src/main/java/org/example/Storage.java
+++ src/main/java/org/example/Storage.java
@@ -2,20 +2,17 @@ package org.example;
 
 import sun.net.ftp.FtpClient;
 
-import javax.lang.model.util.Elements;
-import javax.swing.text.Document;
-import java.io.IOException;
 import java.util.ArrayList;
 
 public class Storage {
     private final ArrayList<String> quoteList;
-    private FtpClient Jsoup;
 
     Storage() {
         quoteList = new ArrayList<>();
         quoteList.add("Начинать всегда стоит с того, что сеет сомнения. \n\nБорис Стругацкий.");
         quoteList.add("80% успеха - это появиться в нужном месте в нужное время.\n\nВуди Аллен");
         quoteList.add("Мы должны признать очевидное: понимают лишь те,кто хочет понять.\n\nБернар Вербер");
+        quoteList.add("Верь в себя и в то что делаешь. \n\nНет автора");
     }
 
     String getRandQuote() {
@@ -25,23 +22,6 @@ public class Storage {
         return quoteList.get(randValue);
     }
 
-//    void parser(String strURL) {
-//        parser("https://citatnica.ru/citaty/mudrye-tsitaty-velikih-lyudej");
-//        String className = "su-note-inner su-u-clearfix su-u-trim";
-//        Document doc = null;
-//        try {
-//            doc = Jsoup.connect(strURL).maxBodySize(0).get();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//
-//        Elements elQuote = doc.getElementsByClass(className);
-//
-//
-//        elQuote.forEach(el -> {
-//                    quoteList.add(el.text());
-//                }
-//        );
-    }
+
+}
 
